cbe5f8b99633469d76e6a1f277df056930c4ad05
elumalai <mynameiselumalai@gmail.com>
2023-03-28 23:54:59 +0530

    Adds Header and material ui packages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import './App.css';
+import "./App.css";
+import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>
+      Lets build a linkedin clone
+
         {/* <Header /> */}
+        <Header />
 
         {/* App Body */}
          {/* side bar */}
          {/* feed */}
          {/* widgets */}
 
-        Lets build a linkedin clone
       </h1>
     </div>
   );
