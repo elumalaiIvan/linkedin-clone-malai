76395c8e41ff8d9c685bcb98da6a446a49f49288
elumalai <mynameiselumalai@gmail.com>
2023-03-29 14:35:18 +0530

    Adds right and left components to the header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import "./App.css";
+import './App.css';
 import Header from './Header';
 
 function App() {
   return (
     <div className="app">
       <h1>
-      Lets build a linkedin clone
-
         {/* <Header /> */}
         <Header />
 
         {/* App Body */}
-         {/* side bar */}
-         {/* feed */}
-         {/* widgets */}
+          {/* side bar */}
+          {/* feed */}
+          {/* widgets */}
 
       </h1>
     </div>
--- src/Header.js
+++ src/Header.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import './Header.css'
 import SearchIcon from '@mui/icons-material/Search';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import HomeIcon from '@mui/icons-material/Home';
+import SupervisorAccountIcon from '@mui/icons-material/SupervisorAccount';
+import HeaderOption from './HeaderOption';
+import { BusinessCenter, Chat, Notifications} from '@mui/icons-material';
+import avatar from './avatar.png'
 
 function Header() {
   return (
     <div className='header'>
-        <h1>This is the Header</h1>
         <div className="header__left">
-            <img src="" alt="" />
+        <LinkedInIcon className="linkedInIcon" />
 
             <div className="header__search">
                 {/* search icon */}
+                
                 <SearchIcon />
                 <input type="text" />
             </div>
@@ -18,6 +24,12 @@ function Header() {
 
         </div>
         <div className="header__right">
+          <HeaderOption Icon={HomeIcon} title="Home" />
+          <HeaderOption Icon={SupervisorAccountIcon} title="My Network" />
+          <HeaderOption Icon={BusinessCenter} title="Jobs" />
+          <HeaderOption Icon={Chat} title="Messaging" />
+          <HeaderOption Icon={Notifications} title="Notifications" />
+          <HeaderOption avatar={avatar} title="me"/>
 
         </div>
 
